5c303525bb121cebd7276c4bac7782090f25eee9
Renaud Mercier <renaud.mercier.dev@gmail.com>
2026-02-19 14:48:50 +0100

    feat:audio full

--- build.bat
+++ build.bat
@@ -1,81 +1,68 @@
 @echo off
 setlocal
 
-REM ===== Chemins =====
 set ROOT=%~dp0
 set SRC=%ROOT%src
 set OUT=%ROOT%build
-
-REM ===== Cree le dossier build si absent =====
-if not exist "%OUT%" mkdir "%OUT%"
-
-REM ===== Windows SDK x64 =====
 set SDK_LIB=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.26100.0\um\x64
 
+if not exist "%OUT%" mkdir "%OUT%"
 pushd "%OUT%"
 
-echo Compilation de audio.asm...
+echo [1/8] audio.asm...
 nasm -f win64 -I"%SRC%\\" "%SRC%\audio.asm" -o audio.obj
 if errorlevel 1 goto :err
 
-echo Compilation de audio_helpers.asm...
-nasm -f win64 -I"%SRC%\\" "%SRC%\audio_helpers.asm" -o audio_helpers.obj
-if errorlevel 1 goto :err
-
-echo Compilation de main.asm...
+echo [2/8] main.asm...
 nasm -f win64 -I"%SRC%\\" "%SRC%\main.asm" -o main.obj
 if errorlevel 1 goto :err
 
-echo Compilation de game.asm...
+echo [3/8] game.asm...
 nasm -f win64 -I"%SRC%\\" "%SRC%\game.asm" -o game.obj
 if errorlevel 1 goto :err
 
-echo Compilation de physics.asm...
+echo [4/8] physics.asm...
 nasm -f win64 -I"%SRC%\\" "%SRC%\physics.asm" -o physics.obj
 if errorlevel 1 goto :err
 
-echo Compilation de input.asm...
+echo [5/8] input.asm...
 nasm -f win64 -I"%SRC%\\" "%SRC%\input.asm" -o input.obj
 if errorlevel 1 goto :err
 
-echo Compilation de platforms.asm...
+echo [6/8] platforms.asm...
 nasm -f win64 -I"%SRC%\\" "%SRC%\platforms.asm" -o platforms.obj
 if errorlevel 1 goto :err
 
-echo Compilation de scroll.asm...
+echo [7/8] scroll.asm...
 nasm -f win64 -I"%SRC%\\" "%SRC%\scroll.asm" -o scroll.obj
 if errorlevel 1 goto :err
 
-echo Compilation de score.asm...
+echo [8/8] score.asm...
 nasm -f win64 -I"%SRC%\\" "%SRC%\score.asm" -o score.obj
 if errorlevel 1 goto :err
 
-REM ===== Link =====
 echo Linkage...
-link main.obj game.obj physics.obj input.obj platforms.obj scroll.obj score.obj audio.obj audio_helpers.obj ^
+link main.obj game.obj physics.obj input.obj platforms.obj scroll.obj score.obj audio.obj ^
  /LIBPATH:"%SDK_LIB%" ^
  kernel32.lib user32.lib gdi32.lib winmm.lib ^
  /SUBSYSTEM:WINDOWS ^
  /ENTRY:Start ^
  /MACHINE:X64 ^
  /OUT:doodle.exe
-
 if errorlevel 1 goto :err
 
 echo.
 echo ============================================
 echo BUILD OK : build\doodle.exe
 echo ============================================
-echo.
 popd
 exit /b 0
 
 :err
 echo.
 echo ============================================
-echo BUILD FAILED - Verifiez les erreurs ci-dessus
+echo BUILD FAILED
 echo ============================================
-echo.
 popd
 pause
 exit /b 1
--- src/audio.asm
+++ src/audio.asm
@@ -1,63 +1,201 @@
-default rel
-bits 64
+BITS 64
+DEFAULT REL
+
+; --- IMPORTS WINAPI ---
+extern waveOutOpen, waveOutPrepareHeader, waveOutWrite
+extern waveOutReset, waveOutClose, waveOutUnprepareHeader
+extern GlobalAlloc, RtlMoveMemory
 
 ; --- EXPORTS ---
 global Audio_Init
 global Audio_Cleanup
+global audio_play_jump
+global audio_update_music
+global audio_stop_music
 
-; --- IMPORTS ---
-extern waveOutOpen, waveOutPrepareHeader, waveOutWrite, waveOutReset, waveOutClose
-extern GlobalAlloc, RtlMoveMemory
-
-; --- INCLUSION DONNÉES SON ---
+; --- INCLUSION MUSIQUE DE FOND ---
 %include "kick_data.inc"
 
+; --- CONSTANTES SON DE SAUT ---
+%define JUMP_RATE       44100
+%define JUMP_SAMPLES    8000
+%define JUMP_HDR_SIZE   44
+%define JUMP_TOTAL      (JUMP_HDR_SIZE + JUMP_SAMPLES)
+
 section .data
+
+    ; ---- Format musique de fond (kick_data.inc) ----
     wfx:
-        dw 1                    
-        dw WavChannels          
-        dd WavRate              
-        dd WavRate * WavChannels * 2 
-        dw WavChannels * 2      
-        dw 16                   
-        dw 0                    
-    
+        dw 1
+        dw WavChannels
+        dd WavRate
+        dd WavRate * WavChannels * 2
+        dw WavChannels * 2
+        dw 16
+        dw 0
+
     TotalBufferSize equ KickSizeBytes * 4
 
+    ; ---- Format son de saut : 44100 Hz, Mono, 8-bit ----
+    wfx_jump:
+        dw 1            ; PCM
+        dw 1            ; Mono
+        dd JUMP_RATE
+        dd JUMP_RATE    ; nAvgBytesPerSec = rate * 1 canal * 1 octet
+        dw 1            ; nBlockAlign
+        dw 8            ; 8 bits
+        dw 0
+
+    ; ---- Constantes FPU pour synthèse "bouing" ----
+    j_start_freq    dq 0.12
+    j_freq_accel    dq 0.00006
+    j_max_vol       dq 110.0
+    j_center        dq 128.0
+    j_decay         dq 0.9995
+
 section .bss
-    hWaveOut     resq 1
-    pBuffer      resq 1
-    waveHdr      resb 64
+    ; ---- Musique de fond ----
+    hWaveOut        resq 1
+    pBuffer         resq 1
+    waveHdr         resb 64
+    music_playing   resd 1
+
+    ; ---- Son de saut ----
+    hWaveOut_jump   resq 1
+    waveHdr_jump    resb 64
+    jump_buffer     resb JUMP_TOTAL
+
+    ; ---- Variables FPU temporaires (synthèse) ----
+    j_angle         resq 1
+    j_freq          resq 1
+    j_vol           resq 1
+    j_temp          resq 1
 
 section .text
 
+%macro zero_header64 1
+    lea     rdi, [rel %1]
+    xor     eax, eax
+    mov     rcx, 8
+%%loop:
+    mov     qword [rdi + rcx*8 - 8], 0
+    dec     rcx
+    jnz     %%loop
+%endmacro
+
 ; =================================================================
-; Audio_Init : Alloue la mémoire, copie le son, lance la lecture
+; start_music_playback (interne)
+; =================================================================
+start_music_playback:
+    push    rbx
+    sub     rsp, 40
+
+    zero_header64 waveHdr
+
+    lea     rbx, [rel waveHdr]
+    mov     rax, [rel pBuffer]
+    mov     [rbx],          rax
+    mov     dword [rbx+8],  TotalBufferSize
+    mov     dword [rbx+24], 12      ; WHDR_BEGINLOOP | WHDR_ENDLOOP
+    mov     dword [rbx+28], -1      ; infini
+
+    mov     rcx, [rel hWaveOut]
+    mov     rdx, rbx
+    mov     r8,  48
+    call    waveOutPrepareHeader
+
+    mov     rcx, [rel hWaveOut]
+    lea     rdx, [rel waveHdr]
+    mov     r8,  48
+    call    waveOutWrite
+
+    mov     dword [rel music_playing], 1
+
+    add     rsp, 40
+    pop     rbx
+    ret
+
+; =================================================================
+; Audio_Init
 ; =================================================================
 Audio_Init:
-    ; Prologue standard
     push    rbp
     mov     rbp, rsp
-    sub     rsp, 48         ; Espace de travail safe
+    sub     rsp, 48
     push    rbx
     push    rdi
 
-    ; 1. Allocation
-    mov     rcx, 0x0040     ; GPTR
+    ; --- 1. SYNTHESE DU SON DE SAUT ---
+    lea rdi, [rel jump_buffer]
+    mov dword [rdi],    0x46464952
+    mov dword [rdi+4],  JUMP_TOTAL - 8
+    mov dword [rdi+8],  0x45564157
+    mov dword [rdi+12], 0x20746d66
+    mov dword [rdi+16], 16
+    mov word  [rdi+20], 1
+    mov word  [rdi+22], 1
+    mov dword [rdi+24], JUMP_RATE
+    mov dword [rdi+28], JUMP_RATE
+    mov word  [rdi+32], 1
+    mov word  [rdi+34], 8
+    mov dword [rdi+36], 0x61746164
+    mov dword [rdi+40], JUMP_SAMPLES
+
+    lea rdi, [rel jump_buffer + JUMP_HDR_SIZE]
+    mov rcx, JUMP_SAMPLES
+
+    finit
+    fldz
+    fstp qword [rel j_angle]
+    fld  qword [rel j_start_freq]
+    fstp qword [rel j_freq]
+    fld  qword [rel j_max_vol]
+    fstp qword [rel j_vol]
+
+.synth_loop:
+    fld  qword [rel j_angle]
+    fsin
+    fld  qword [rel j_vol]
+    fmulp st1, st0
+    fld  qword [rel j_center]
+    faddp st1, st0
+    fistp qword [rel j_temp]
+    mov  al, byte [rel j_temp]
+    mov  [rdi], al
+
+    fld  qword [rel j_angle]
+    fadd qword [rel j_freq]
+    fstp qword [rel j_angle]
+
+    fld  qword [rel j_freq]
+    fadd qword [rel j_freq_accel]
+    fstp qword [rel j_freq]
+
+    fld  qword [rel j_vol]
+    fmul qword [rel j_decay]
+    fstp qword [rel j_vol]
+
+    inc  rdi
+    dec  rcx
+    jnz  .synth_loop
+
+    finit
+
+    ; --- 2. ALLOCATION MUSIQUE DE FOND ---
+    mov     rcx, 0x0040
     mov     rdx, TotalBufferSize
     call    GlobalAlloc
     test    rax, rax
     jz      .Fail
-    mov     [pBuffer], rax
+    mov     [rel pBuffer], rax
 
-    ; 2. Copie (4 boucles)
     mov     rdi, rax
     mov     rbx, 4
-.FillLoop:
+.fill_loop:
     mov     rcx, rdi
     lea     rdx, [KickData]
-    mov     r8, KickSizeBytes
-    push    rdi             ; Sauvegarde registres volatils
+    mov     r8,  KickSizeBytes
+    push    rdi
     push    rbx
     sub     rsp, 32
     call    RtlMoveMemory
@@ -66,36 +204,29 @@ Audio_Init:
     pop     rdi
     add     rdi, KickSizeBytes
     dec     rbx
-    jnz     .FillLoop
+    jnz     .fill_loop
 
-    ; 3. Open
-    lea     rcx, [hWaveOut]
+    ; --- 3. DEVICE MUSIQUE ---
+    lea     rcx, [rel hWaveOut]
     mov     rdx, -1
-    lea     r8, [wfx]
-    xor     r9, r9
+    lea     r8,  [rel wfx]
+    xor     r9,  r9
     mov     qword [rsp+32], 0
     mov     qword [rsp+40], 0
     call    waveOutOpen
     test    rax, rax
     jnz     .Fail
 
-    ; 4. Prepare & Play
-    lea     rbx, [waveHdr]
-    mov     rax, [pBuffer]
-    mov     [rbx], rax
-    mov     dword [rbx+8], TotalBufferSize
-    mov     dword [rbx+24], 12  ; LOOP
-    mov     dword [rbx+28], -1  ; INFINI
-    
-    mov     rcx, [hWaveOut]
-    mov     rdx, rbx
-    mov     r8, 48
-    call    waveOutPrepareHeader
+    call    start_music_playback
 
-    mov     rcx, [hWaveOut]
-    lea     rdx, [waveHdr]
-    mov     r8, 48
-    call    waveOutWrite
+    ; --- 4. DEVICE SON DE SAUT (handle séparé) ---
+    lea     rcx, [rel hWaveOut_jump]
+    mov     rdx, -1
+    lea     r8,  [rel wfx_jump]
+    xor     r9,  r9
+    mov     qword [rsp+32], 0
+    mov     qword [rsp+40], 0
+    call    waveOutOpen
 
 .Fail:
     pop     rdi
@@ -105,16 +236,120 @@ Audio_Init:
     ret
 
 ; =================================================================
-; Audio_Cleanup : Arrête tout
+; Audio_Cleanup
 ; =================================================================
 Audio_Cleanup:
     sub     rsp, 40
-    mov     rcx, [hWaveOut]
+
+    mov     rcx, [rel hWaveOut]
     test    rcx, rcx
-    jz      .Done
+    jz      .cleanup_jump
     call    waveOutReset
-    mov     rcx, [hWaveOut]
+    mov     rcx, [rel hWaveOut]
+    lea     rdx, [rel waveHdr]
+    mov     r8,  48
+    call    waveOutUnprepareHeader
+    mov     rcx, [rel hWaveOut]
     call    waveOutClose
-.Done:
+    mov     qword [rel hWaveOut], 0
+
+.cleanup_jump:
+    mov     rcx, [rel hWaveOut_jump]
+    test    rcx, rcx
+    jz      .done
+    call    waveOutReset
+    mov     rcx, [rel hWaveOut_jump]
+    lea     rdx, [rel waveHdr_jump]
+    mov     r8,  48
+    call    waveOutUnprepareHeader
+    mov     rcx, [rel hWaveOut_jump]
+    call    waveOutClose
+    mov     qword [rel hWaveOut_jump], 0
+
+.done:
     add     rsp, 40
+    ret
+
+; =================================================================
+; audio_stop_music
+; =================================================================
+audio_stop_music:
+    sub     rsp, 40
+
+    cmp     dword [rel music_playing], 0
+    je      .done
+
+    mov     rcx, [rel hWaveOut]
+    test    rcx, rcx
+    jz      .done
+    call    waveOutReset
+
+    mov     rcx, [rel hWaveOut]
+    lea     rdx, [rel waveHdr]
+    mov     r8,  48
+    call    waveOutUnprepareHeader
+
+    mov     dword [rel music_playing], 0
+
+.done:
+    add     rsp, 40
+    ret
+
+; =================================================================
+; audio_update_music
+; =================================================================
+audio_update_music:
+    sub     rsp, 40
+
+    cmp     dword [rel music_playing], 1
+    je      .done
+
+    call    start_music_playback
+
+.done:
+    add     rsp, 40
+    ret
+
+; =================================================================
+; audio_play_jump
+; reset -> unprepare -> zero -> prepare -> write a chaque appel
+; =================================================================
+audio_play_jump:
+    push    rbx
+    push    rdi
+    sub     rsp, 40
+
+    mov     rbx, [rel hWaveOut_jump]
+    test    rbx, rbx
+    jz      .done
+
+    mov     rcx, rbx
+    call    waveOutReset
+
+    mov     rcx, rbx
+    lea     rdx, [rel waveHdr_jump]
+    mov     r8,  48
+    call    waveOutUnprepareHeader
+
+    zero_header64 waveHdr_jump
+
+    lea     rdi, [rel waveHdr_jump]
+    lea     rax, [rel jump_buffer]
+    mov     [rdi],         rax
+    mov     dword [rdi+8], JUMP_TOTAL
+
+    mov     rcx, rbx
+    mov     rdx, rdi
+    mov     r8,  48
+    call    waveOutPrepareHeader
+
+    mov     rcx, rbx
+    lea     rdx, [rel waveHdr_jump]
+    mov     r8,  48
+    call    waveOutWrite
+
+.done:
+    add     rsp, 40
+    pop     rdi
+    pop     rbx
     ret
